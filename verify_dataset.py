097281d80bb9b0578f8279e00b931adf93d2a854
thomaoc <thomas.ocuilleanain@ulb.be>
2025-03-27 11:30:33 +0100

    dataset verif

---
+++ verify_dataset.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import torchvision.transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+
+
+if __name__ == '__main__':
+    data = ImageFolder("dataset/deepfake-dataset/train", transform=torchvision.transforms.ToTensor())
+    loader = DataLoader(data)
+
+    for X, y in loader:
+        plt.imshow(X.squeeze().permute(1, 2, 0))
+        plt.show()
+        break
+
+
