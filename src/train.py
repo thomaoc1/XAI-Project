eed241d0f7244288c3659b6b316f41874c3747ea
thomaoc <thomas.ocuilleanain@ulb.be>
2025-03-28 10:27:09 +0100

    imbalance model

--- src/train.py
+++ src/train.py
@@ -1,26 +1,43 @@
 import torch
 import torch.nn.functional as F
 import tqdm
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
+import numpy as np
 
 from src.classifier import DeepFakeClassifier
 
 def init_dataloader(batch_size, dev, nw):
     dataset = ImageFolder("dataset/deepfake-dataset/train", transform=transforms.ToTensor())
+
+    dominant_class = 0
+    min_class = 1
+    downsampling_factor = 3
+
+    targets = np.array(dataset.targets)
+    dominant_indices = np.where(targets == dominant_class)[0]
+    min_class_indices = np.where(targets == min_class)[0]
+
+    downsampled_dominant_indices = np.random.choice(dominant_indices, len(dominant_indices) // downsampling_factor, replace=False)
+    final_indices = np.concatenate([downsampled_dominant_indices, min_class_indices])
+
+    sampler = SubsetRandomSampler(final_indices)
+
+
     loader = DataLoader(
         dataset,
         batch_size=batch_size,
         num_workers=nw,
-        shuffle=True,
+        sampler=sampler,
         pin_memory=dev == 'cuda'
     )
-    return loader
+
+    return loader, downsampling_factor
 
 
 def train(num_epochs: int, batch_size: int, nw: int, dev: str):
-    loader = init_dataloader(batch_size, dev, nw)
+    loader, downsample_factor = init_dataloader(batch_size, dev, nw)
 
     model = DeepFakeClassifier().to(dev)
     optimiser = torch.optim.Adam(model.parameters())
@@ -32,8 +49,11 @@ def train(num_epochs: int, batch_size: int, nw: int, dev: str):
         for img, label in progress_bar:
             img, label = img.to(dev), label.float().to(dev)
 
+            weight = torch.ones_like(label, device=dev)
+            weight[label == 0] = downsample_factor
+
             preds = model(img).squeeze(1)
-            loss = F.binary_cross_entropy_with_logits(preds, label)
+            loss = F.binary_cross_entropy_with_logits(preds, label, weight=weight)
 
             optimiser.zero_grad()
             loss.backward()
@@ -45,7 +65,7 @@ def train(num_epochs: int, batch_size: int, nw: int, dev: str):
         avg_loss = epoch_loss / len(loader)
         print(f"Epoch {epoch + 1}/{num_epochs} - Avg Loss: {avg_loss:.4f}")
 
-    torch.save(model.state_dict(), 'model.pt')
+    torch.save(model.state_dict(), 'imbalance-model.pt')
 
 
 if __name__ == '__main__':
