43c9b6e2cee16672de477a8f8bc73e69278f5739
thomaoc <thomas.ocuilleanain@ulb.be>
2025-03-28 17:21:46 +0100

    made priv attr public

--- src/classifier.py
+++ src/classifier.py
@@ -5,8 +5,8 @@ from torchvision.models import ResNet50_Weights, resnet50
 class DeepFakeClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self._backbone = resnet50(weights=ResNet50_Weights.DEFAULT)
-        self._backbone.fc = nn.Linear(self._backbone.fc.in_features, 1)
+        self.backbone = resnet50(weights=ResNet50_Weights.DEFAULT)
+        self.backbone.fc = nn.Linear(self.backbone.fc.in_features, 1)
 
     def forward(self, x):
         return self._backbone(x)
