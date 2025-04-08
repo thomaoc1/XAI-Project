0d084d052c0a4068a5de8548398363fdc2cf11f7
thomaoc <thomas.ocuilleanain@ulb.be>
2025-04-08 19:51:03 +0200

    simplified generate dataset

--- src/datagen/generate_dataset.py
+++ src/datagen/generate_dataset.py
@@ -1,14 +1,15 @@
+import argparse
+
 import cv2
-import time
 import numpy as np
 from skimage.feature import hog
 import torch
-import torchattacks
 from pytorch_grad_cam import GradCAM
 from tqdm import tqdm
 
 from src.classification.binary_classifier import BinaryClassifier
 from src.classification.train import init_dataloader
+from src.config import DatasetConfig
 
 
 def extract_heatmap_features(correct_heatmaps: np.ndarray):
@@ -49,106 +50,45 @@ def extract_heatmap_features(correct_heatmaps: np.ndarray):
     return torch.tensor(np.stack(all_features, axis=0), dtype=torch.float32)
 
 
-def main(model_path: str, batch_size: int, adv: bool = False):
+def main(cfg: DatasetConfig, batch_size: int):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     num_workers = 4 if device == 'cuda' else 0
-    loader = init_dataloader(batch_size=batch_size, dev=device, nw=num_workers)
+
+    loader = init_dataloader(
+        cfg.get_classifier_dataset_split('train'),
+        batch_size=batch_size,
+        nw=num_workers,
+        transform=cfg.get_classifier_transform(),
+        pin_memory=device == 'cuda'
+    )
 
     model = BinaryClassifier()
-    model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    model.load_state_dict(torch.load(cfg.get_classifier_save_path(), map_location=device, weights_only=True))
     model.eval()
-
-    attack = torchattacks.FGSM(model)
 
     target_layers = [model.backbone.layer4[-1]]
 
     heatmaps = []
-    all_labels = []
-    all_vanilla_preds = []
-    all_adv_preds = []
-    all_ground_truths = []
     with GradCAM(model=model, target_layers=target_layers) as cam:
-        progress_bar = tqdm(loader, desc="Processing batches", unit="batch")
-
-        for img, label in progress_bar:
+        for img, label in tqdm(loader, desc="Processing batches", unit="batch"):
             img, label = img.to(device), label.to(device)
-
             grayscale_cam = cam(input_tensor=img)
-            vanilla_preds: torch.Tensor = cam.outputs.argmax(dim=1)
-
-            if adv:
-                adv_img = attack(img, label)
-                grayscale_cam_adv = cam(input_tensor=adv_img)
-                adv_preds: torch.Tensor = cam.outputs.argmax(dim=1)
-
-                batch_ground_truths = torch.cat([label, label])
-                batch_vanilla_preds = torch.cat([vanilla_preds, vanilla_preds])
-                batch_heatmaps = torch.cat([torch.tensor(grayscale_cam), torch.tensor(grayscale_cam_adv)])
-                batch_adv_preds = torch.cat([torch.ones(batch_size, dtype=torch.int) * -1, adv_preds])
-                batch_labels = torch.cat(
-                    [
-                        torch.zeros(batch_size, dtype=torch.int),  # VANILLA
-                        torch.ones(batch_size, dtype=torch.int)  # ADVERSARIAL
-                    ]
-                )
-
-                all_vanilla_preds.append(batch_vanilla_preds)
-                all_adv_preds.append(batch_adv_preds)
-                all_ground_truths.append(batch_ground_truths)
-                all_labels.append(batch_labels)
-            else:
-                batch_heatmaps = torch.tensor(grayscale_cam)
-
+            batch_heatmaps = torch.tensor(grayscale_cam)
             heatmaps.append(batch_heatmaps)
-            print(heatmaps)
 
-            metrics = {
-                'Vanilla Acc': (vanilla_preds == label.cpu()).float().mean().item()
-            }
-
-            if adv:
-                metrics.update(
-                    {
-                        'Adv Acc': (adv_preds == label.cpu()).float().mean().item(),
-                        'Flip Rate': (vanilla_preds != adv_preds).float().mean().item()
-                    }
-                )
-
-            progress_bar.set_postfix(metrics)
-
-    heatmaps = torch.cat(heatmaps, dim=0)
-    obj = {
-        'heatmaps': heatmaps,
-    }
-
-    if adv:
-        heatmap_labels = torch.cat(all_labels, dim=0)
-        classification = torch.cat(all_vanilla_preds, dim=0)
-        ground_truths = torch.cat(all_ground_truths, dim=0)
-        adv_classification = torch.cat(all_adv_preds, dim=0)
-        obj.update(
-            {
-                'heatmaps': heatmaps,
-                'labels': heatmap_labels,
-                'model_vanilla_preds': classification,
-                'ground_truth': ground_truths,
-                'model_adv_preds': adv_classification,
-            }
-        )
-
-    torch.save(
-        obj, 'deep_fake_hm_dataset.pt'
+    torch.save({
+        'heatmaps': torch.cat(heatmaps, dim=0),
+        },
+        cfg.get_heatmap_dataset_path()
     )
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Generate Heatmap Dataset")
+    parser.add_argument('dataset', type=str, choices=['deepfake', 'dogs-vs-cats'])
+    parser.add_argument('--batch_size', type=int, default=64)
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    main(model_path='new_model.pt', batch_size=64)
-
-
-
-
-
-
-
-
-
-
+    args = parse_args()
+    config = DatasetConfig(args.dataset)
+    main(cfg=config, batch_size=args.batch_size)
