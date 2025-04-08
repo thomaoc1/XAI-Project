c0e6d468f496ce123202a9d609791fdbc1ebb8af
thomaoc <thomas.ocuilleanain@ulb.be>
2025-04-08 13:33:00 +0200

    simplified args for vae train

--- src/anomalydetection/train.py
+++ src/anomalydetection/train.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -26,23 +29,32 @@ def loss_function(recon_x, x, mu, logvar):
     return recon_loss + kl_loss
 
 
-def main():
+def get_paths(dataset_name: str, attack_name: str):
+    dataset_path = os.path.join('dataset', 'heatmap', f'{dataset_name}_{attack_name.lower()}_hm_dataset.pt')
+    save_path = os.path.join('model', f'{dataset_name}_{attack_name}_hm_vae.pt')
+    return dataset_path, save_path
+
+
+
+
+def main(dataset_name: str, attack_name: str, n_epochs: int, batch_size: int):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     vae = CNNVAE(latent_dim=128).to(device)
     optimizer = torch.optim.Adam(vae.parameters(), lr=1e-3)
-    num_epochs = 50
+
+    dataset_path, save_path = get_paths(dataset_name, attack_name)
 
     loader = init_dataloader(
-        'deep_fake_hm_dataset.pt',
-        batch_size=64,
+        dataset_path,
+        batch_size=batch_size,
         pin_memory=device == 'cuda',
         nw=4 if device == 'cuda' else 0
     )
 
-    for epoch in tqdm(range(num_epochs), desc="Epochs"):
+    for epoch in tqdm(range(n_epochs), desc="Epochs"):
         epoch_loss = 0.0
-        for hm in tqdm(loader, desc=f"Epoch {epoch}", leave=False):
+        for hm in loader:
             hm = hm.to(device).unsqueeze(1)
             optimizer.zero_grad()
             recon_x, mu, logvar = vae(hm)
@@ -56,9 +68,23 @@ def main():
 
     torch.save(
         vae.state_dict(),
-        'cnn_vae_model.pt'
+        save_path,
     )
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train VAE on heatmap dataset")
+    parser.add_argument('dataset', type=str, choices=['deepfake', 'cats_vs_dogs'])
+    parser.add_argument('attack', type=str, choices=["FGSM", "PGD"])
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--batch_size', type=int, default=64)
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(
+        dataset_name=args.dataset,
+        attack_name=args.attack,
+        n_epochs=args.epochs,
+        batch_size=args.batch_size,
+    )
