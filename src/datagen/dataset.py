89babec286d522c608eddd8dc90e6f71535c3cea
thomaoc <thomas.ocuilleanain@ulb.be>
2025-04-13 09:14:58 +0200

    Added condition for heatmaps stored in bytes

--- src/datagen/dataset.py
+++ src/datagen/dataset.py
@@ -5,9 +5,13 @@ from torch.utils.data import Dataset
 class UnsupervisedHeatmapDataset(Dataset):
     def __init__(self, heatmaps: torch.Tensor):
         self.heatmaps = heatmaps
+        self._is_normalised = heatmaps.max().item() <= 1.0
 
     def __len__(self):
         return len(self.heatmaps)
 
     def __getitem__(self, idx):
-        return self.heatmaps[idx].unsqueeze(0)
+        if self._is_normalised:
+            return self.heatmaps[idx].unsqueeze(0)
+        else:
+            return self.heatmaps[idx].unsqueeze(0) / 255.0
